4dd2fbcfb73819914cda89dda82654ee65fe64bb
John Dang <john.dang@trainadesign.com>
2019-05-29 17:02:50 -0700

    js30 day 11

---
+++ 10 - Hold Shift and Check Checkboxes/js/script.js
@@ -0,0 +1,28 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+// console.log(checkboxes);
+
+function handleCheck(e) {
+  // console.log(e);
+
+  // check if they had the shift key down
+  // AND check that they are checking it
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    // go ahead and do what we please
+    // loop over every single checkbox
+    checkboxes.forEach(checkbox => {
+      console.log(checkboxes);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log("Starting to check them inbetween!");
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener("click", handleCheck));
